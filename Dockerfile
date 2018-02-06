9ab7abcbb4bcf84403c1cf732237fe975d5f5865
ashleigh simonelli <ashleighsimonelli@gmail.com>
2018-02-06 14:46:02 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM navidonskis/nginx-php7.1
+
+ARG DOCUMENTATION_NAME[='*.php']
+ARG DOCUMENTATION_EXCLUDES[='']
+ARG DOCUMENTATION_DIR[='/var/www']
+ARG DOCUMENTATION_THEME[='']
+ARG DOCUMENTATION_BUILD_DIR[='/var/www']
+ARG DOCUMENTATION_CACHE_DIR[='']
+ARG DOCUMENTATION_GITHUB[='']
+ARG DOCUMENTATION_OPENED_LEVEL[=2]
+
+RUN mkdir /sami
+
+ADD files/composer.json /sami/composer.json
+ADD files/composer.lock /sami/composer.lock
+ADD files/sami.config.php /sami/sami.config.php
+
+RUN cd /sami && curl -O http://get.sensiolabs.org/sami.phar
+
+RUN ls -l /sami
+
+RUN composer install -d /sami
+
+RUN php /sami/sami.phar update /sami/sami.config.php -v
+
+WORKDIR /var/www
---
+++ files/sami.config.php
@@ -0,0 +1,30 @@
+<?php
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+//use Dotenv\Dotenv;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+
+//$env = new Dotenv(__DIR__);
+//
+//$env->load();
+
+$iterator = Finder::create()
+    ->files()
+    ->name(getenv('DOCUMENTATION_NAME', '*.php'));
+
+$excludes = explode(',', getenv('DOCUMENTATION_EXCLUDES', 'vendor'));
+
+foreach ($excludes as $exclude) {
+    $iterator->exclude($exclude);
+}
+
+$iterator->in($dir = getenv('DOCUMENTATION_DIR', ''));
+
+return new Sami($iterator, [
+    'theme' => getenv('DOCUMENTATION_THEME', 'default'),
+    'build_dir' => getenv('DOCUMENTATION_BUILD_DIR', '/var/www/'),
+    'cache_dir' => getenv('DOCUMENTATION_CACHE_DIR', '/cache'),
+    'remote_repository' => new GitHubRemoteRepository(getenv('DOCUMENTATION_GITHUB', ''), dirname($dir)),
+    'default_opened_level' => getenv('DOCUMENTATION_OPENED_LEVEL', 2),
+]);
