7060453453472e7e655b069ca0af8798c955770a
ashleigh simonelli <ashleighsimonelli@gmail.com>
2018-02-07 16:53:11 +0000

    Removed composer setup

--- files/sami.config.php
+++ files/sami.config.php
@@ -2,29 +2,56 @@
 
 use Sami\Sami;
 use Symfony\Component\Finder\Finder;
-//use Dotenv\Dotenv;
 use Sami\RemoteRepository\GitHubRemoteRepository;
 
-//$env = new Dotenv(__DIR__);
-//
-//$env->load();
+/**
+ * @param $key
+ * @param null $default
+ * @return array|bool|false|string|void
+ */
+function env($key, $default = null)
+{
+    $value = getenv($key);
+    if ($value === false) {
+        return $default;
+    }
+    switch (strtolower($value)) {
+        case 'true':
+        case '(true)':
+            return true;
+        case 'false':
+        case '(false)':
+            return false;
+        case 'empty':
+        case '(empty)':
+            return '';
+        case 'null':
+        case '(null)':
+            return;
+    }
+
+    return $value;
+}
 
 $iterator = Finder::create()
     ->files()
-    ->name(getenv('DOCUMENTATION_NAME', '*.php'));
+    ->name(env('DOCUMENTATION_NAME', '*.php'));
 
-$excludes = explode(',', getenv('DOCUMENTATION_EXCLUDES', 'vendor'));
+$excludes = explode(',', env('DOCUMENTATION_EXCLUDES', 'vendor'));
 
 foreach ($excludes as $exclude) {
     $iterator->exclude($exclude);
 }
 
-$iterator->in($dir = getenv('DOCUMENTATION_DIR', '/var/project/'));
+$iterator->in($dir = env('DOCUMENTATION_DIR', '/var/project/'));
 
-return new Sami($iterator, [
-    'theme' => getenv('DOCUMENTATION_THEME', 'default'),
-    'build_dir' => getenv('DOCUMENTATION_BUILD_DIR', '/var/www/'),
-    'cache_dir' => getenv('DOCUMENTATION_CACHE_DIR', '/cache'),
-    'remote_repository' => new GitHubRemoteRepository(getenv('DOCUMENTATION_GITHUB', ''), dirname($dir)),
-    'default_opened_level' => getenv('DOCUMENTATION_OPENED_LEVEL', 2),
-]);
+$config = [
+    'theme' => env('DOCUMENTATION_THEME', 'default'),
+    'build_dir' => env('DOCUMENTATION_BUILD_DIR', '/var/www/'),
+    'cache_dir' => env('DOCUMENTATION_CACHE_DIR', '/cache'),
+    'default_opened_level' => env('DOCUMENTATION_OPENED_LEVEL', 2),
+];
+
+if (env('DOCUMENTATION_GITHUB', false) !== false) $config['remote_repository'] = new GitHubRemoteRepository(env('DOCUMENTATION_GITHUB', ''), dirname($dir));
+
+return new Sami($iterator, $config);
